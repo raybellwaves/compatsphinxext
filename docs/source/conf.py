0f8aa5bf665c9a6c7ab52c9a904d4e03edffb282
Ray Bell <rayjohnbell0@gmail.com>
2024-06-03 00:47:18 +0000

    combine lines

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,6 @@ extensions = [
     "sphinx.ext.autosummary",
     "sphinx.ext.githubpages",
     "sphinx.ext.intersphinx",
-    "sphinx.ext.linkcode",
     "PandasCompat",
     "todo",
 ]
@@ -61,65 +60,6 @@ intersphinx_mapping = {
         "https://numpy.org/doc/stable/objects.inv",
     ),
 }
-
-
-# based on numpy doc/source/conf.py
-def linkcode_resolve(domain, info) -> str | None:
-    """
-    Determine the URL corresponding to Python object
-    """
-    if domain != "py":
-        return None
-
-    modname = info["module"]
-    fullname = info["fullname"]
-
-    submod = sys.modules.get(modname)
-    if submod is None:
-        return None
-
-    obj = submod
-    for part in fullname.split("."):
-        try:
-            with warnings.catch_warnings():
-                # Accessing deprecated objects will generate noisy warnings
-                warnings.simplefilter("ignore", FutureWarning)
-                obj = getattr(obj, part)
-        except AttributeError:
-            return None
-
-    try:
-        fn = inspect.getsourcefile(inspect.unwrap(obj))
-    except TypeError:
-        try:  # property
-            fn = inspect.getsourcefile(inspect.unwrap(obj.fget))
-        except (AttributeError, TypeError):
-            fn = None
-    if not fn:
-        return None
-
-    try:
-        source, lineno = inspect.getsourcelines(obj)
-    except TypeError:
-        try:  # property
-            source, lineno = inspect.getsourcelines(obj.fget)
-        except (AttributeError, TypeError):
-            lineno = None
-    except OSError:
-        lineno = None
-
-    if lineno:
-        linespec = f"#L{lineno}-L{lineno + len(source) - 1}"
-    else:
-        linespec = ""
-
-    mylib = pathlib.Path(__file__).parents[2].resolve().as_posix()
-    fn = os.path.relpath(fn, start=mylib)
-
-    return (
-        f"https://github.com/raybellwaves/{mylib}/blob/"
-        f"main/{fn}{linespec}"
-    )
 
 
 def setup(app):
